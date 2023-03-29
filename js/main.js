322805558b4302b3e778633eb42950b8482ecbaf
Orixena <encephalon666@yandex.ru>
2023-03-29 22:54:26 +0300

    удалила лишнее из main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,4 @@ import './picture.js';
 import './form.js';
 import './scale.js';
 import './effects.js';
-import { thumbnailRender } from './miniature.js';
-//import { setUserFormSubmit, closeUploadForm } from './form.js';
 
-
-
-// fetch('https://28.javascript.pages.academy/kekstagram/data')
-//   .then((response) => response.json())
-//   .then((images) => {
-//     console.log(images);
-//     thumbnailRender(images);
-//   });
-
-//setUserFormSubmit(closeUploadForm);
