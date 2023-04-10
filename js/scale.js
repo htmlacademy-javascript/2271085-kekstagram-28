0ae522bb855c7e99c587821aa5deab8f4fb71ad3
Orixena <encephalon666@yandex.ru>
2023-04-10 18:17:09 +0300

    правки после 1 проверки

--- js/scale.js
+++ js/scale.js
@@ -14,7 +14,7 @@ const changeScale = (value) => {
   scaleValueElement.value = `${value}%`;
 };
 
-const onMakeSmaller = () => {
+const onSmallerMake = () => {
   actualScale -= SCALE_STEP;
   if(actualScale < SCALE_STEP){
     actualScale = SCALE_STEP;
@@ -23,7 +23,7 @@ const onMakeSmaller = () => {
   changeScale(actualScale);
 };
 
-const onMakeBigger = () => {
+const onBiggerMake = () => {
   actualScale += SCALE_STEP;
   if(actualScale >= SCALE_MAX){
     actualScale = SCALE_MAX;
@@ -34,7 +34,7 @@ const onMakeBigger = () => {
 
 const resetScale = () => changeScale(SCALE_DEFAULT);
 
-scaleSmallerButtonElement.addEventListener('click', onMakeSmaller);
-scaleBiggerButtonElement.addEventListener('click', onMakeBigger);
+scaleSmallerButtonElement.addEventListener('click', onSmallerMake);
+scaleBiggerButtonElement.addEventListener('click', onBiggerMake);
 
 export {resetScale};
--- js/util.js
+++ js/util.js
@@ -23,13 +23,13 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-function debounce (callback, timeoutDelay = 500) {
+const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
 
   return (...rest) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
-}
+};
 
 export {isEscapeKey,showAlert,debounce};
